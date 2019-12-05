8ac06bf14e15fb123e863557407e9119236b6c64
Lucas <47852219+glori4n@users.noreply.github.com>
2019-12-05 15:37:00 +0000

    Update index.php

--- index.php
+++ index.php
@@ -125,4 +125,4 @@ echo "Nothing sent yet.";
 
 <br>
 <br>
-<footer style="text-align:center">This snippet was made by Glori4n (<a href="https://glori4n.com" target="new">https://glori4n.com</a>)</footer>
+<footer style="text-align:center">This snippet was made by © Glori4n (<a href="https://glori4n.com" target="new">https://glori4n.com</a>)</footer>
